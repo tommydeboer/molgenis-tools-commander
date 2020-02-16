fdb0e1ee4bf4dec8e34c41ab4aa4d53ba031a9bc
tommydeboer <tommydeboer4@gmail.com>
2020-02-16 20:04:05 +0100

    add permission service and permission manager client

---
+++ mcmd/molgenis/service/_client/permission_manager.py
@@ -0,0 +1,47 @@
+from urllib.parse import urljoin
+
+from mcmd.molgenis.service._client import api
+from mcmd.molgenis.service._client.client import post_form
+from mcmd.molgenis.service.permissions import Permission
+from mcmd.molgenis.service.system import User, Role, EntityType, Package, Plugin
+
+
+def grant_user_entity_type_permission(user: User, permission: Permission, entity_type: EntityType):
+    data = {'radio-' + entity_type.id: permission.value(),
+            'username': user.username}
+    _grant(data, 'entityclass', 'user')
+
+
+def grant_user_package_permission(user: User, permission: Permission, package: Package):
+    data = {'radio-' + package.id: permission.value(),
+            'username': user.username}
+    _grant(data, 'package', 'user')
+
+
+def grant_user_plugin_permission(user: User, permission: Permission, plugin: Plugin):
+    data = {'radio-' + plugin.id: permission.value(),
+            'username': user.username}
+    _grant(data, 'plugin', 'user')
+
+
+def grant_role_entity_type_permission(role: Role, permission: Permission, entity_type: EntityType):
+    data = {'radio-' + entity_type.id: permission.value(),
+            'rolename': role.name}
+    _grant(data, 'entityclass', 'user')
+
+
+def grant_role_package_permission(role: Role, permission: Permission, package: Package):
+    data = {'radio-' + package.id: permission.value(),
+            'rolename': role.name}
+    _grant(data, 'package', 'user')
+
+
+def grant_role_plugin_permission(role: Role, permission: Permission, plugin: Plugin):
+    data = {'radio-' + plugin.id: permission.value(),
+            'rolename': role.name}
+    _grant(data, 'plugin', 'user')
+
+
+def _grant(data: dict, resource_name: str, principal_name: str):
+    url = urljoin(api.permissions(), '{}/{}'.format(resource_name, principal_name))
+    post_form(url, data)
---
+++ mcmd/molgenis/service/permissions.py
@@ -0,0 +1,73 @@
+from enum import Enum
+
+from mcmd.molgenis.service._client import permission_manager
+from mcmd.molgenis.service.system import User, Role, Principal, PermissableResource, EntityType, Package, Plugin, Entity
+
+
+class Permission(Enum):
+    COUNT = 'count',
+    READ_META = 'readmeta'
+    READ = 'read',
+    WRITE = 'write'
+    WRITEMETA = 'writemeta'
+
+
+def give_permission(principal: Principal, permission: Permission, resource: PermissableResource):
+    if isinstance(principal, User):
+        if isinstance(resource, EntityType):
+            give_user_entity_type_permission(principal, permission, resource)
+        elif isinstance(resource, Package):
+            give_user_package_permission(principal, permission, resource)
+        elif isinstance(resource, Plugin):
+            give_user_plugin_permission(principal, permission, resource)
+        elif isinstance(resource, Entity):
+            give_user_entity_permission(principal, permission, resource)
+        else:
+            raise ValueError('unknown PermissableResource type')
+    elif isinstance(principal, Role):
+        if isinstance(resource, EntityType):
+            give_role_entity_type_permission(principal, permission, resource)
+        elif isinstance(resource, Package):
+            give_role_package_permission(principal, permission, resource)
+        elif isinstance(resource, Plugin):
+            give_role_plugin_permission(principal, permission, resource)
+        elif isinstance(resource, Entity):
+            give_role_entity_permission(principal, permission, resource)
+        else:
+            raise ValueError('unknown PermissableResource type')
+    else:
+        raise ValueError('unknown Principal type')
+
+
+def give_user_entity_type_permission(user: User, permission: Permission, entity_type: EntityType):
+    permission_manager.grant_user_entity_type_permission(user, permission, entity_type)
+
+
+def give_user_package_permission(user: User, permission: Permission, package: Package):
+    permission_manager.grant_user_package_permission(user, permission, package)
+
+
+def give_user_plugin_permission(user: User, permission: Permission, plugin: Plugin):
+    permission_manager.grant_user_plugin_permission(user, permission, plugin)
+
+
+# noinspection PyUnusedLocal
+def give_user_entity_permission(user: User, permission: Permission, entity: Entity):
+    raise NotImplementedError('row level permissions not yet implemented')
+
+
+def give_role_entity_type_permission(role: Role, permission: Permission, entity_type: EntityType):
+    permission_manager.grant_role_entity_type_permission(role, permission, entity_type)
+
+
+def give_role_package_permission(role: Role, permission: Permission, package: Package):
+    permission_manager.grant_role_package_permission(role, permission, package)
+
+
+def give_role_plugin_permission(role: Role, permission: Permission, plugin: Plugin):
+    permission_manager.grant_role_plugin_permission(role, permission, plugin)
+
+
+# noinspection PyUnusedLocal
+def give_role_entity_permission(role: Role, permission: Permission, entity: Entity):
+    raise NotImplementedError('row level permissions not yet implemented')
